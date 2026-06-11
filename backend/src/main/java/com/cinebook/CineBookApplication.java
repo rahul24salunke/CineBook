69f565bdfd0dc147908068ed930c5a0fb0c3c256
VemalaDhanunjay <167599834+VemalaDhanunjay@users.noreply.github.com>
2026-06-11 15:07:01 +0530

    chore: add server startup message to console output

--- backend/src/main/java/com/cinebook/CineBookApplication.java
+++ backend/src/main/java/com/cinebook/CineBookApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CineBookApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CineBookApplication.class, args);
         System.out.println("server started at port 8181");
